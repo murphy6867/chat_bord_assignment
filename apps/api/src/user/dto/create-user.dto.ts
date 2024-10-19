71397568ea779c13332a507bdff2bc9ae761b815
murphy6867 <sonsawan.n1994@gmail.com>
2024-10-19 12:27:07 +0700

    create: Sign-up api

---
+++ apps/api/src/user/dto/create-user.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  username: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
