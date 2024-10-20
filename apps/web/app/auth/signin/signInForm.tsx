d5789db3f6fdaeb9e84668f9a265d7e3edeae12f
murphy6867 <sonsawan.n1994@gmail.com>
2024-10-20 11:06:05 +0700

    hotfix: hiddent not use page

--- apps/web/app/auth/signin/signInForm.tsx
+++ apps/web/app/auth/signin/signInForm.tsx
@@ -1,43 +1,51 @@
 "use client";
 
 import { FC } from "react";
-import { useFormState } from "react-dom";
+// import { useFormState } from "react-dom";
 import Link from "next/link";
 
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import SubmitButton from "@/components/ui/submitButton";
 
-import { signUp } from "@/lib/auth";
+// import { signUp } from "@/lib/auth";
 
-const SignInForm: FC = () => {
+const SignUpForm: FC = () => {
   
-  const [state, action] = useFormState(signUp, undefined);
 
   return (
     <div className=" min-w-80 lg:w-1/2 flex flex-col space-y-8">
-      <h1 className="text-3xl font-bold text-white">Sign In</h1>
-      <form action={action} className="flex flex-col space-y-4 px-4">
-        {state?.message && (
+      <h1 className="text-3xl font-bold text-white">Sign up</h1>
+      <form  className="flex flex-col space-y-4 px-4">
+        {/* {state?.message && (
           <p className="text-sm text-red-500">{state.message}</p>
-        )}
+        )} */}
 
-        <div>
-          <Label htmlFor="name">Name: </Label>
-          <Input id="name" name="name" placeholder="Input your Name" />
-          {state?.error?.name && (
-            <p className="text-sm text-red-500">{state.error?.name}</p>
-          )}
+        <div className="space-y-2">
+          <Label htmlFor="username" className="text-white">Username: </Label>
+          <Input id="username" name="username" placeholder="Input your Username" className="bg-white" />
+          {/* {state?.error?.username && (
+            <p className="text-sm text-red-500">{state.error?.username}</p>
+          )} */}
         </div>
 
-        <div>
-          <Label htmlFor="password">Password: </Label>
+        <div className="space-y-2">
+          <Label htmlFor="email" className="text-white">Email: </Label>
+          <Input id="email" name="email" placeholder="Input your Email" className="bg-white" />
+          {/* {state?.error?.email && (
+            <p className="text-sm text-red-500">{state.error?.email}</p>
+          )} */}
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="password" className="text-white">Password: </Label>
           <Input
             id="password"
             name="password"
             placeholder="Input your Password"
+            className="bg-white"
           />
-          {state?.error?.password && (
+          {/* {state?.error?.password && (
             <div className="text-sm text-red-500">
               <p>Password must:</p>
               <ul>
@@ -46,14 +54,14 @@ const SignInForm: FC = () => {
                 ))}
               </ul>
             </div>
-          )}
+          )} */}
         </div>
 
         <SubmitButton>Sign In</SubmitButton>
-
+        
         <p className="text-center text-white text-sm">
           Haven't an account?{" "}
-          <Link href={"/auth/signup"} className="font-bold">
+          <Link href={"/auth/signin"} className="font-bold">
             sign up
           </Link>
         </p>
@@ -62,4 +70,4 @@ const SignInForm: FC = () => {
   );
 };
 
-export default SignInForm;
+export default SignUpForm;
