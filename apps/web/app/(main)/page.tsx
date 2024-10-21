d2955edd7598094d522af4d6a0bdec04c52cfbc9
murphy6867 <sonsawan.n1994@gmail.com>
2024-10-21 18:36:57 +0700

    reverse: hot-fix

--- apps/web/app/(main)/page.tsx
+++ apps/web/app/(main)/page.tsx
@@ -1,18 +1,26 @@
 import { FC } from "react";
 
 import MainContent from "@/components/layout/MainContent";
-import { BACKEND_URL } from "@/lib/constants";
+// import { BACKEND_URL } from "@/lib/constants";
 
 const HomePage: FC = async () => {
-  const res = await fetch(`${BACKEND_URL}/posts`);
-  const posts = await res.json();
-console.log('====================================');
-console.log(JSON.stringify(posts));
-console.log('====================================');
+  // const res = await fetch(`${BACKEND_URL}/posts`);
+  // const posts = await res.json();
+  const posts = [
+    {
+      id: 1,
+      title: "NestJS",
+      content: "Best Backend",
+      user_id: 1,
+      category_id: 7,
+      created_at: "2024-10-21T07:33:25.552Z",
+      updated_at: "2024-10-21T07:33:25.552Z",
+    },
+  ];
   return (
     <div className="flex flex-1 min-h-screen overflow-hidden">
       <div>
-        <MainContent contents={posts}/>
+        <MainContent contents={posts} />
       </div>
     </div>
   );
